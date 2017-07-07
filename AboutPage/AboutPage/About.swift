d855ec3721167232e26ca77ebd2ef5812098ff7a
gabrielcodex <gabriel.breshears@gmail.com>
2017-07-07 11:07:22 -0400

    Solution created

--- AboutPage/AboutPage/About.swift
+++ AboutPage/AboutPage/About.swift
@@ -12,7 +12,10 @@ import Foundation
 struct About {
     //Write your code here. Make sure to put all of your code in between the two curly braces that surround the About struct
 
- 
+    let name: String = "Gabriel Breshears"
+    let age: String = "275"
+    let favoriteMovies: [String] = ["Wonder Woman", "Eternal Sunshine of the Spotless Mind", "Princess Mononoke"]
+    let biography: String = "I was born in New York city but rasied in the South. Now I'm back and ready to code!"
     
     
 }
